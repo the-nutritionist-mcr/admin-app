17572132252242043ca2cd8ae10cddf8c881001e
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-11-08 18:19:59 +0000

    Remove commented out code

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -66,10 +66,3 @@ export const plans: Plan[] = [
     costPerMeal: 635,
   },
 ];
-
-// export const plans = [
-//   { label: "micro", value: "150" },
-//   { label: "min", value: "250" },
-//   { label: "max", value: "350" },
-//   { label: "mass", value: "450" },
-// ];
