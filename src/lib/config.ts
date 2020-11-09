5d1f52c632930c1f0ba3106bbb7f818f46df2a15
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-11-09 09:52:25 +0000

    Change config order

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,33 +2,23 @@ import Plan from "../domain/Plan";
 
 export type PlanCategory = "Mass" | "EQ" | "Micro" | "Ultra-Micro";
 
-export const daysPerWeekOptions = [5, 6];
+export const daysPerWeekOptions = [6, 5];
 
 export const plans: Plan[] = [
-  {
-    category: "Mass",
-    mealsPerDay: 3,
-    costPerMeal: 885,
-  },
-  {
-    category: "Mass",
-    mealsPerDay: 2,
-    costPerMeal: 885,
-  },
   {
     category: "Mass",
     mealsPerDay: 1,
     costPerMeal: 902,
   },
   {
-    category: "EQ",
-    mealsPerDay: 3,
-    costPerMeal: 760,
+    category: "Mass",
+    mealsPerDay: 2,
+    costPerMeal: 885,
   },
   {
-    category: "EQ",
-    mealsPerDay: 2,
-    costPerMeal: 760,
+    category: "Mass",
+    mealsPerDay: 3,
+    costPerMeal: 885,
   },
   {
     category: "EQ",
@@ -36,14 +26,14 @@ export const plans: Plan[] = [
     costPerMeal: 787,
   },
   {
-    category: "Micro",
-    mealsPerDay: 3,
-    costPerMeal: 663,
+    category: "EQ",
+    mealsPerDay: 2,
+    costPerMeal: 760,
   },
   {
-    category: "Micro",
-    mealsPerDay: 2,
-    costPerMeal: 663,
+    category: "EQ",
+    mealsPerDay: 3,
+    costPerMeal: 760,
   },
   {
     category: "Micro",
@@ -51,9 +41,19 @@ export const plans: Plan[] = [
     costPerMeal: 673,
   },
   {
-    category: "Ultra-Micro",
+    category: "Micro",
+    mealsPerDay: 2,
+    costPerMeal: 663,
+  },
+  {
+    category: "Micro",
     mealsPerDay: 3,
-    costPerMeal: 625,
+    costPerMeal: 663,
+  },
+  {
+    category: "Ultra-Micro",
+    mealsPerDay: 1,
+    costPerMeal: 635,
   },
   {
     category: "Ultra-Micro",
@@ -62,7 +62,7 @@ export const plans: Plan[] = [
   },
   {
     category: "Ultra-Micro",
-    mealsPerDay: 1,
-    costPerMeal: 635,
+    mealsPerDay: 3,
+    costPerMeal: 625,
   },
 ];
