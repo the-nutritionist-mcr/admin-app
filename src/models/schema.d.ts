2c04ffaf211e825847382eb63ad4326bf34a5a52
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-12-04 21:59:52 +0000

    Delete models

--- src/models/schema.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { Schema } from "@aws-amplify/datastore";
-
-export declare const schema: Schema;
-/* eslint-enable import/prefer-default-export */
