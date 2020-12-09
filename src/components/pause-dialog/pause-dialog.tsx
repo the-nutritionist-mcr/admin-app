e0f5d84e14d6d9b818f372dd8ee5015ab152a1c5
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-12-09 19:51:23 +0000

    Add days of week to pause dialog

--- src/components/pause-dialog/pause-dialog.tsx
+++ src/components/pause-dialog/pause-dialog.tsx
@@ -73,7 +73,7 @@ const PauseDialog: React.FC<PauseDialogProps> = (props) => {
               <DateInput
                 inline={true}
                 name="pauseStart"
-                calendarProps={{ size: "small" }}
+                calendarProps={{ size: "small", daysOfWeek: true }}
               />
               <Box pad={{ top: "small" }}>
                 <Text alignSelf="center">
@@ -88,6 +88,7 @@ const PauseDialog: React.FC<PauseDialogProps> = (props) => {
                 inline={true}
                 name="pauseEnd"
                 calendarProps={{
+                  daysOfWeek: true,
                   size: "small",
                 }}
               />
