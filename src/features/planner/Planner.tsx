727c5ee07400e9fa39f40758f28647d099cdf921
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-12-16 17:27:24 +0000

    Tweak layout

--- src/features/planner/Planner.tsx
+++ src/features/planner/Planner.tsx
@@ -1,19 +1,17 @@
-import { Box, Button, Heading, Paragraph, Select } from "grommet";
+import { Box, Button, Heading, Paragraph, Select, Tab, Tabs } from "grommet";
 import {
   LOCALSTORAGE_KEY_DAY,
   LOCALSTORAGE_KEY_PLANNED,
 } from "../../lib/constants";
-import { chooseMeals, makePlan } from "../../lib/plan-meals";
 
 import DeliveryDay from "../../types/DeliveryDay";
 import DeliveryMealsSelection from "../../types/DeliveryMealsSelection";
 import React from "react";
 import Recipe from "../../domain/Recipe";
-import ToCookTable from "./ToCookTable";
 import ToPackTable from "./ToPackTable";
 import { allCustomersSelector } from "../../features/customers/customersSlice";
 import { allRecipesSelector } from "../../features/recipes/recipesSlice";
-import styled from "styled-components";
+import { chooseMeals } from "../../lib/plan-meals";
 import { useSelector } from "react-redux";
 
 const defaultPlans: DeliveryMealsSelection = [
@@ -24,18 +22,6 @@ const defaultPlans: DeliveryMealsSelection = [
   undefined,
   undefined,
 ];
-
-const HiddenFromPrintBox = styled(Box)`
-  @media print {
-    display: none;
-  }
-`;
-
-const PrintableBox = styled(Box)`
-  @media print {
-    display: block;
-  }
-`;
 
 const Planner: React.FC = () => {
   const [day, setDay] = React.useState<DeliveryDay>(
@@ -50,16 +36,28 @@ const Planner: React.FC = () => {
   const recipes = useSelector(allRecipesSelector);
 
   const chosenMeals = chooseMeals(day, planned, customers);
-  const cookPlan = makePlan(chosenMeals);
 
   const activeSelections = planned.filter(Boolean);
 
   return (
     <React.Fragment>
       <Heading level={2}>Planner</Heading>
-      <HiddenFromPrintBox>
-        <Paragraph fill>
-          Planning for{" "}
+      <Tabs alignControls="start" margin={"0"}>
+        <Tab title="Choose">
+          <Paragraph>
+            Welcome to the delivery planner. Please note that data in this
+            section is stored <strong>locally</strong> only.
+          </Paragraph>
+          <Paragraph>
+            Use the selection boxes below to choose what delivery day you are
+            planning for and what meals you would like to include in the
+            delivery.
+          </Paragraph>
+          <Paragraph>
+            When you are ready, click the <strong>next</strong> button to
+            generate a delivery plan, or <strong>clear</strong> to start again
+          </Paragraph>
+          <Heading level={3}>Delivery Day</Heading>
           <Select
             placeholder="Select Day"
             name="selectDay"
@@ -70,63 +68,54 @@ const Planner: React.FC = () => {
               localStorage.setItem(LOCALSTORAGE_KEY_DAY, event.value);
             }}
           />
-        </Paragraph>
-        <Paragraph fill>Select the meals for this delivery:</Paragraph>
-        <Box direction="row" gap="medium">
-          {planned.map((plan, index) => (
-            <Select
-              key={index}
-              name={`meal-${index}`}
-              options={recipes}
-              placeholder="None"
-              value={plan ?? ""}
-              labelKey={(labelPlan: undefined | Recipe): string | undefined =>
-                labelPlan?.name
-              }
-              onChange={(event: { value: Recipe | undefined }): void => {
-                const newPlanned = [...planned];
-                newPlanned[index] = recipes.find(
-                  (recipe) => recipe.id === event?.value?.id
-                );
-                localStorage.setItem(
-                  LOCALSTORAGE_KEY_PLANNED,
-                  JSON.stringify(newPlanned.map((item) => item?.id))
-                );
-                setPlanned(newPlanned);
-              }}
-            />
-          ))}
-          {activeSelections.length > 0 || day !== "" ? (
-            <Button
-              onClick={(): void => {
-                setDay("");
-                setPlanned(defaultPlans);
-                localStorage.setItem(
-                  LOCALSTORAGE_KEY_PLANNED,
-                  JSON.stringify(defaultPlans)
-                );
-              }}
-              label="Clear"
-            />
-          ) : null}
-        </Box>
-      </HiddenFromPrintBox>
-      <PrintableBox>
-        {activeSelections.length === defaultPlans.length && day !== "" ? (
-          <React.Fragment>
-            <ToCookTable plan={cookPlan} />
-            <ToPackTable
-              customerMeals={chosenMeals}
-              deliveryMeals={defaultPlans}
-            />
-          </React.Fragment>
-        ) : (
-          <Paragraph fill>
-            To calculate your delivery plan, please choose all{" "}
-            {defaultPlans.length} recipes
-          </Paragraph>
-        )}
-      </PrintableBox>
+          <Heading level={3}>Meals</Heading>
+          <Box direction="column" gap="small" align="start">
+            {planned.map((plan, index) => (
+              <Select
+                key={index}
+                name={`meal-${index}`}
+                options={recipes}
+                placeholder="None"
+                value={plan ?? ""}
+                labelKey={(labelPlan: undefined | Recipe): string | undefined =>
+                  labelPlan?.name
+                }
+                onChange={(event: { value: Recipe | undefined }): void => {
+                  const newPlanned = [...planned];
+                  newPlanned[index] = recipes.find(
+                    (recipe) => recipe.id === event?.value?.id
+                  );
+                  localStorage.setItem(
+                    LOCALSTORAGE_KEY_PLANNED,
+                    JSON.stringify(newPlanned.map((item) => item?.id))
+                  );
+                  setPlanned(newPlanned);
+                }}
+              />
+            ))}
+            {activeSelections.length > 0 || day !== "" ? (
+              <Button
+                onClick={(): void => {
+                  setDay("");
+                  setPlanned(defaultPlans);
+                  localStorage.setItem(
+                    LOCALSTORAGE_KEY_PLANNED,
+                    JSON.stringify(defaultPlans)
+                  );
+                }}
+                label="Clear"
+              />
+            ) : null}
+          </Box>
+        </Tab>
+        <Tab title="Allocate">
+          <ToPackTable
+            customerMeals={chosenMeals}
+            deliveryMeals={defaultPlans}
+          />
+        </Tab>
+        <Tab title="Plan"></Tab>
+      </Tabs>
     </React.Fragment>
   );
 };
