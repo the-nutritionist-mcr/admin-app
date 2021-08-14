3646edf7b4ff14e05987cd4e4270f0f1061338e6
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-08-14 23:08:26 +0100

    ci: added semantic-release config

--- release.config.js
+++ release.config.js
@@ -6,6 +6,19 @@
         "@semantic-release/release-notes-generator", 
         {
           "preset": "conventionalcommits",
+          "releaseRules": [
+            { "type": "feat", "release":"minor" },
+            { "type": "fix", "release": "patch" },
+            { "type": "docs", "release": false },
+            { "type": "style", "release": false },
+            { "type": "refactor", "release": false },
+            { "type": "perf", "release": "patch" },
+            { "type": "test", "release": false },
+            { "type": "build", "release": false },
+            { "type": "ci", "release": false },
+            { "type": "chore", "release": false },
+            { "type": "revert", "release": "patch" },
+          ]
         }
       ],
       [
