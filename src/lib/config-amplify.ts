9de92dc1571cd302676388dfcfce094f9d42a437
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-08-18 22:10:46 +0100

    fix: backend and frontend were temporarily mismatched

--- src/lib/config-amplify.ts
+++ src/lib/config-amplify.ts
@@ -1,26 +1,31 @@
 import Amplify from "@aws-amplify/core";
 import { Auth } from "@aws-amplify/auth";
-import backendConfig from "../backend-outputs.json";
 
-const stackConfigKey =
-  Object.keys(backendConfig).find((key) => key.includes("BackendStack")) ?? "";
+export const configAmplify = async (): Promise<void> => {
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const configObject = (backendConfig as any)[stackConfigKey];
+  const configResponse = await fetch(`${process.env.PUBLIC_URL}/backend-outputs.json`)
+  const backendConfig = await configResponse.json()
 
-const config = {
-  /* eslint-disable @typescript-eslint/naming-convention */
-  Auth: {
-    mandatorySignIn: true,
-    region: "us-east-1",
-    userPoolId: configObject.UserPoolId,
-    userPoolWebClientId: configObject.ClientId,
-  },
-  aws_appsync_graphqlEndpoint: configObject.GraphQlQpiUrl,
-  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
-  graphql_endpoint_iam_region: "us-east-1",
-  /* eslint-enable @typescript-eslint/naming-convention */
-};
+  const stackConfigKey =
+    Object.keys(backendConfig).find((key) => key.includes("BackendStack")) ?? "";
 
-Amplify.configure(config);
-Auth.configure(config);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const configObject = (backendConfig as any)[stackConfigKey];
+
+  const config = {
+    /* eslint-disable @typescript-eslint/naming-convention */
+    Auth: {
+      mandatorySignIn: true,
+      region: "us-east-1",
+      userPoolId: configObject.UserPoolId,
+      userPoolWebClientId: configObject.ClientId,
+    },
+    aws_appsync_graphqlEndpoint: configObject.GraphQlQpiUrl,
+    aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+    graphql_endpoint_iam_region: "us-east-1",
+    /* eslint-enable @typescript-eslint/naming-convention */
+  };
+
+  Amplify.configure(config);
+  Auth.configure(config);
+}
