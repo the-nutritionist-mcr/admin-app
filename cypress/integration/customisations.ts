bdc533bbdd7980c73bd7fd8c9bf3f1a9e56cb618
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-08-23 22:11:02 +0100

    test: reinstaed customisations e2e test

---
+++ cypress/integration/customisations.ts
@@ -0,0 +1,70 @@
+import { COGNITO_PASSWORD, COGNITO_USER } from "../support/constants";
+
+describe("The customisations page", () => {
+
+  before(() => {
+    cy.seed();
+  });
+
+  describe("when logged in", () => {
+
+    beforeEach(() => {
+      cy.loginByCognitoApi(COGNITO_USER, COGNITO_PASSWORD);
+      cy.visit("/");
+      cy.contains("Customisations").click();
+    });
+
+  it("should render a page with the correct title", () => {
+    cy.get("h2").contains("Customisations");
+  });
+
+  it("should render the empty data message on first page load", () => {
+    cy.get("body").should("not.contain", "table");
+    cy.contains("You've not added any customisations yet...");
+  });
+
+  it("should allow you to add and edit a couple of customisations which persist after page reload", () => {
+    cy.createCustomisation("Chillis", false);
+    cy.createCustomisation("Nuts", true);
+    cy.createCustomisation("Fish", false);
+
+    cy.get("tbody").find("tr").first().as("firstRow");
+    cy.get("@firstRow").contains("Chillis");
+    cy.get("@firstRow").contains("No");
+
+    cy.get("tbody").find("tr").first().next().as("secondRow");
+    cy.get("@secondRow").contains("Fish");
+    cy.get("@secondRow").contains("No");
+
+    cy.get("tbody").find("tr").last().as("lastRow");
+    cy.get("@lastRow").contains("Nuts");
+    cy.get("@lastRow").contains("Yes");
+  });
+
+  it("should allow you to create customisations that can be added to recipes", () => {
+    cy.createRecipe("Special Salad", "A lovely salad", ["Chillis"], "salad");
+    cy.get("tbody").find("tr").first().contains("Chillis");
+  });
+
+  // it("should allow you to create customisations that can be added to customers", () => {
+  //   cy.createCustomer(
+  //     "Mr",
+  //     "Ben",
+  //     "Wainwright",
+  //     1,
+  //     "bwainwright28@gmail.com",
+  //     "Mass 3",
+  //     5,
+  //     "Large",
+  //     true,
+  //     "Somewhere",
+  //     ["Nuts"],
+  //     "Some notes",
+  //     4,
+  //     "0123456789"
+  //   );
+
+  //   cy.get("tbody").find("tr").contains("Nuts");
+  // });
+  });
+});
