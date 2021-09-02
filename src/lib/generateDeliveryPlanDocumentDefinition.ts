905ce209ab35cc06d13c44a6239499e6ee21aeb2
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-09-02 20:00:44 +0100

    feat: don't break pack plan up by delivery days

--- src/lib/generateDeliveryPlanDocumentDefinition.ts
+++ src/lib/generateDeliveryPlanDocumentDefinition.ts
@@ -6,7 +6,7 @@ import {
   CustomerMealsSelection,
   SelectedItem,
 } from "../lib/plan-meals";
-import { daysOfWeek, defaultDeliveryDays } from "./config";
+import { defaultDeliveryDays } from "./config";
 import formatPlanItem from "./formatPlanItem";
 import { PdfBuilder } from "./pdf-builder";
 
@@ -15,10 +15,6 @@ const COLUMNS = 6;
 interface CustomerMealDaySelection {
   customer: Customer;
   delivery: SelectedItem[];
-}
-
-interface CookSelections {
-  [day: string]: CustomerMealDaySelection[];
 }
 
 const makeRowsFromSelections = (
@@ -38,22 +34,6 @@ const makeRowsFromSelections = (
       .map((item) => formatPlanItem(item.mealWithVariantString, item)),
   ]);
 
-const daySelections = (
-  cookIndex: number,
-  day: string,
-  selections: CustomerMealsSelection
-) =>
-  selections
-    .filter(
-      (selection) =>
-        selection.customer.newPlan?.configuration.deliveryDays[cookIndex] ===
-        day
-    )
-    .map(({ deliveries, ...selection }) => ({
-      ...selection,
-      delivery: deliveries[cookIndex],
-    }));
-
 const generateNameString = (customer: Customer) =>
   `${customer.surname}, ${customer.firstName}`;
 
@@ -68,18 +48,6 @@ const generateDeliveryPlanDocumentDefinition = (
   selections: CustomerMealsSelection,
   allMeals: Recipe[]
 ): DocumentDefinition => {
-  const customerGroups = defaultDeliveryDays.map((day, cookIndex) =>
-    daysOfWeek.reduce<CookSelections>(
-      (current, dayOfWeek) =>
-        daySelections(cookIndex, dayOfWeek, selections).length === 0
-          ? current
-          : {
-              ...current,
-              [dayOfWeek]: daySelections(cookIndex, dayOfWeek, selections),
-            },
-      {}
-    )
-  );
 
   const date = new Date(Date.now());
 
@@ -89,18 +57,16 @@ const generateDeliveryPlanDocumentDefinition = (
     options as any
   )})`;
 
-  const builder = customerGroups.reduce<PdfBuilder>(
-    (topBuilder, current, cookIndex) =>
-      Object.entries(current).reduce<PdfBuilder>(
-        (midBuilder, [day, currentSelections]) =>
-          midBuilder
-            .header(`Cook ${cookIndex + 1}: ${day}`)
-            .table(makeRowsFromSelections(currentSelections, allMeals), COLUMNS)
-            .pageBreak(),
-        topBuilder
-      ),
-    new PdfBuilder(title, true)
-  );
+
+  const builder = defaultDeliveryDays.reduce<PdfBuilder>(
+    (topBuilder, current, cookIndex) => {
+      const daySelections = selections.map(({ customer, deliveries }) => ({ customer, delivery: deliveries[cookIndex]}));
+      return topBuilder
+            .header(`Cook ${cookIndex + 1}`)
+            .table(makeRowsFromSelections(daySelections, allMeals), COLUMNS)
+            .pageBreak();
+    },
+    new PdfBuilder(title, true))
 
   return builder.toDocumentDefinition();
 };
